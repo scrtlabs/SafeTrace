dc0a6f610cd2eabdbdf22d6c61b39848885076fc
Pandeya <Nutan.Pandeya@McKesson.com>
2020-04-19 20:34:36 -0700

    Added Sign up pages UI template

--- UIcode/client/src/components/signout.js
+++ UIcode/client/src/components/signout.js
@@ -2,23 +2,40 @@ import React, { Component } from 'react';
 import Header from './header'
 import {Card,Button} from 'react-bootstrap';
 class signout extends React.Component {
-    render(){
-        return(
-            <div><Header /> 
-            <div className="centeralign">
-            <Card border="light" style={{ width: '18rem' }}>
-           
-            <Card.Body>
-                <Card.Title>You have successfully logged out!</Card.Title>
-                <Card.Text>
-                 
-                </Card.Text>
-                <Button variant="primary">Login Again !</Button>
-            </Card.Body>
-            </Card>
-            
+    render() {
+        return (
+            <div>
+            <Header />
+             <div className="loginBlock"> 
+            <form>
+                <h3>Sign Up</h3>
+
+                <div className="form-group">
+                    <label>First name</label>
+                    <input type="text" className="form-control" placeholder="First name" />
+                </div>
+
+                <div className="form-group">
+                    <label>Last name</label>
+                    <input type="text" className="form-control" placeholder="Last name" />
+                </div>
+
+                <div className="form-group">
+                    <label>Email address</label>
+                    <input type="email" className="form-control" placeholder="Enter email" />
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" />
+                </div>
+
+                <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
+                <p className="forgot-password text-right">
+                    Already registered <a href="/signin">sign in?</a>
+                </p>
+            </form>
             </div>
-            
             </div>
         );
     }
